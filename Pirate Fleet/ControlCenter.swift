22ff61336f5d2a3d4b9822712e1a440e8202dcda
Shivang Bhandari <pump382156@gmail.com>
2017-05-27 18:42:24 +0530

    mines added

--- Pirate Fleet/ControlCenter.swift
+++ Pirate Fleet/ControlCenter.swift
@@ -17,21 +17,31 @@ struct Ship {
     let isVertical: Bool
 }
 
+struct Mine: _Mine_ {
+    let location: GridLocation
+    let explosionText: String
+}
+
 class ControlCenter {
     
     func addShipsAndMines(human: Human) {
+        
         let mediumShip1 = Ship(length: 3, location: GridLocation(x: 0, y: 0), isVertical: false)
         let mediumShip2 = Ship(length: 3, location: GridLocation(x: 0, y: 1), isVertical: false)
         let smallShip1 = Ship(length: 2, location: GridLocation(x: 0, y: 2), isVertical: false)
         let largeShip1 = Ship(length: 4, location: GridLocation(x: 0, y: 3), isVertical: false)
         let xlargeShip1 = Ship(length: 5, location: GridLocation(x: 0, y: 4), isVertical: false)
-
+        let mine1 = Mine(location: GridLocation(x: 5, y: 0), explosionText: "Boom!! Hit1")
+        let mine2 = Mine(location: GridLocation(x: 5, y: 3), explosionText: "Skadoosh")
+        
+        
         human.addShipToGrid(mediumShip1)
         human.addShipToGrid(mediumShip2)
         human.addShipToGrid(smallShip1)
         human.addShipToGrid(largeShip1)
         human.addShipToGrid(xlargeShip1)
-
+        human.addMineToGrid(mine1)
+        human.addMineToGrid(mine2)
 
 
     }
